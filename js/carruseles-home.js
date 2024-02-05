fc0a39252c4bf23e0121ac4bdc5efedfbc4a7e4e
luistope98 <33384695+luistope98@users.noreply.github.com>
2024-02-05 16:51:45 -0500

    html, CSs y js iniciales

---
+++ js/carruseles-home.js
@@ -0,0 +1,34 @@
+
+
+
+var swiper = new Swiper(".Interes", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    slidesPerView:2,
+    autoplay: true,
+    freeMode: true,
+    speed: 1000,
+    loop: true,
+    rewind: true,
+
+  });
+
+
+
+  
+  var swiper3 = new Swiper(".Banner", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    slidesPerView:1,
+    autoplay: true,
+    freeMode: true,
+    speed: 1000,
+    loop: true,
+    rewind: true,
+    effect:'fade',
+  
+  });
