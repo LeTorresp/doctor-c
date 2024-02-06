bbf8ce0f98460495b65f46d5a90de74844c4df2a
luistope98 <33384695+luistope98@users.noreply.github.com>
2024-02-06 07:57:00 -0500

    responsive style

--- js/carruseles-home.js
+++ js/carruseles-home.js
@@ -8,13 +8,19 @@ var swiper = new Swiper(".Interes", {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
   },
-  slidesPerView: 3,
-  autoplay: true,
-  freeMode: true,
-  spaceBetween: 50,
-  speed: 1000,
-  rewind: true,
-
+  
+  slidesPerView: 1,
+  spaceBetween: 20,
+    autoplay: true,
+    freeMode: true,
+    speed: 1000,
+    rewind: true,
+    breakpoints: {
+      900: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
 });
 
 var swiper3 = new Swiper(".Banner", {
