34ff638379b32b32ee06e22edc21725a94a522d0
luistope98 <33384695+luistope98@users.noreply.github.com>
2024-02-06 02:11:52 -0500

    arreglo bug carruseles

--- js/carruseles-home.js
+++ js/carruseles-home.js
@@ -1,34 +1,39 @@
 
-
-
 var swiper = new Swiper(".Interes", {
-    pagination: {
+  pagination: {
       el: ".swiper-pagination",
       dynamicBullets: true,
-    },
-    slidesPerView:2,
-    autoplay: true,
-    freeMode: true,
-    speed: 1000,
-    loop: true,
-    rewind: true,
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+  slidesPerView: 3,
+  autoplay: true,
+  freeMode: true,
+  spaceBetween: 50,
+  speed: 1000,
+  rewind: true,
 
-  });
+});
 
-
-
-  
-  var swiper3 = new Swiper(".Banner", {
-    pagination: {
+var swiper3 = new Swiper(".Banner", {
+  pagination: {
       el: ".swiper-pagination",
       dynamicBullets: true,
-    },
-    slidesPerView:1,
-    autoplay: true,
-    freeMode: true,
-    speed: 1000,
-    loop: true,
-    rewind: true,
-    effect:'fade',
-  
-  });
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+  slidesPerView: 1,
+  autoplay: true,
+  freeMode: true,
+  speed: 1000,
+  rewind: true,
+  effect: 'fade',
+
+});
+
+
+
